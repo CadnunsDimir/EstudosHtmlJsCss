caab42a68dfdb8edcaf4b5a0bf11f1e794ad0119
Tiago Silva do Nascimento <cadnuns.dimir@gmail.com>
2015-11-25 13:56:20 -0200

    montando crud para manipular localStorage

--- scripts/classes.js
+++ scripts/classes.js
@@ -1,4 +1,21 @@
 //definição de classes
 function LocalStorageAcess(){
-
+  
+  var allTasks = [];
+  
+  var _loadStorage = function(){
+    
+  };
+  allTasks = _loadStorage();
+  this.ListAllTasks = function(){
+    
+  };
+  
+  this.ListTodoTasks = function(){
+    
+  };
+  
+  this.CreateTask = function(taskObj){
+    
+  };
 }
