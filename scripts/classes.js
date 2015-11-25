3cc12951c038ce6ebac14bca349c7b6a83b665e7
Tiago Silva do Nascimento <cadnuns.dimir@gmail.com>
2015-11-25 16:15:46 -0200

    Update classes.js

--- scripts/classes.js
+++ scripts/classes.js
@@ -1,14 +1,16 @@
 //definição de classes
 function LocalStorageAcess(){
+  var _storageKey = "uauhskjhkjas";
   
   var allTasks = [];
   
   var _loadStorage = function(){
-    
+    return localStorage.getItem(_storageKey);
   };
   allTasks = _loadStorage();
   this.ListAllTasks = function(){
-    
+    allTasks = _loadStorage();
+    return allTasks;
   };
   
   this.ListTodoTasks = function(){
@@ -16,6 +18,12 @@ function LocalStorageAcess(){
   };
   
   this.CreateTask = function(taskObj){
-    
+    localStorage.setItem(_storageKey,JSON.stringify(taskObj));
+  };
+  this.ReadTask = function(id){
+    var taskObj = localStorage.getItem(_storageKey);
+    return = JSON.parse(taskObj);
   };
 }
+
+var localStorageAcess = new LocalStorageAcess();
